e4e1dc2287de63b5a5a2c8bc5c7097579c4b313b
Martin <mr.mmaurer+work@gmail.com>
2021-10-13 23:56:55 +0100

    Improved name extracting

--- scripts/map.ts
+++ scripts/map.ts
@@ -1,24 +1,43 @@
 import { pipe } from "fp-ts/function";
+import * as O from "fp-ts/lib/Option";
+
 import type { RawElement } from "../types";
 
 import royalsJSON from "../dist/royals.json";
 
 const royals: RawElement[] = royalsJSON;
 
-// function mapName(name: string): string {
-//   return name.trim();
-// }
+const values = (element: RawElement) => {
+  return element.values;
+};
+
+const fullName = (valuesParam: RawElement["values"]) => {
+  return valuesParam[0];
+};
 
 export async function mapEntries(): Promise<void> {
   const [firstEntry] = royals;
 
+  // const [firstEntryTest] = royals[555];
+
+  // const names = pipe(royals, Array.map(square));
+
   const name = pipe(
-    firstEntry,
-    (entry) => entry.values,
-    (values) => values[0],
-    (value) => value.match(/: (.*)/),
-    (results) => (results === null ? "" : results[1])
+    O.fromNullable(firstEntry),
+    O.map(values),
+    O.map(fullName),
+    // O.chain(values),
+    // O.chain(fullName),
+    O.getOrElse(() => "")
   );
+
+  // const name = pipe(
+  //   firstEntry,
+  //   (entry) => entry.values,
+  //   (values) => values[0],
+  //   (value) => value.match(/: (.*)/),
+  //   (results) => (results === null ? "" : results[1])
+  // );
 
   console.log(name);
 }
